998ebeb682d633c2a265cdbbf3a69c08e8792183
Selin Karabıçak <selinss62@gmail.com>
2023-04-24 10:38:57 +0300

    Last Update

--- consumerhome.js
+++ consumerhome.js
@@ -59,3 +59,4 @@ function startTime() {
       label.innerText = value
     }
   }
+  
--- producerlogin.php
+++ producerlogin.php
@@ -2,25 +2,25 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Consumer Login</title>
+    <title>Producer Login</title>
     <link rel="stylesheet" href="login.css">
   </head>
   <body>
     <div class="wrapper">
-      <div class="title">Consumer Login Page</div>
+      <div class="title">Producer Login <br> Page</div>
       <form action="#">
         <div class="field">
           <input type="text" required>
-          <label>Email Address</label>
+          <label>Username</label>
         </div>
         <div class="field">
           <input type="password" required>
           <label>Password</label>
         </div>
+       
         <div class="field">  
-          <input type="submit" onclick="window.location.href='./consumerprofile.html';" value="Login" style = "background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);color: #fff;">
+          <input type="submit" onclick="window.location.href='./producerprofile.html';" value="Login">
         </div>
-        <div class="signup-link">Not a member? <a href="consumersignup.php">Signup now</a></div>
       </form>
     </div>
   </body>
