4afb98df2519fa3fc36e5500c1df61d1d30c4431
Selin Karabıçak <selinss62@gmail.com>
2023-06-11 21:20:58 +0300

    Fixed href part

--- consumerlogin.php
+++ consumerlogin.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Consumer Login</title>
-    <link rel="stylesheet" href="login.css">
-  </head>
-  <body>
-    <div class="wrapper">
-      <div class="title">Consumer Login Page</div>
-      <form action="#">
-        <div class="field">
-          <input type="text" required>
-          <label>Email Address</label>
-        </div>
-        <div class="field">
-          <input type="password" required>
-          <label>Password</label>
-        </div>
-        <div class="field">  
-          <input type="submit" onclick="window.location.href='./consumerprofile.html';" value="Login" style = "background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);color: #fff;">
-        </div>
-        <div class="signup-link">Not a member? <a href="consumersignup.php">Signup now</a></div>
-      </form>
-    </div>
-  </body>
+
+<head>
+  <meta charset="utf-8">
+  <title>Consumer Login</title>
+  <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="title">Consumer Login Page</div>
+    <form action="#">
+      <div class="field">
+        <input type="text" required>
+        <label>Email Address</label>
+      </div>
+      <div class="field">
+        <input type="password" required>
+        <label>Password</label>
+      </div>
+      <div class="field">
+        <input type="submit" onclick="window.location.href='./consumerprofile.php';" value="Login"
+          style="background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);color: #fff;">
+      </div>
+      <div class="signup-link">Not a member? <a href="consumersignup.php">Signup now</a></div>
+    </form>
+  </div>
+</body>
+
 </html>
